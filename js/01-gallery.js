d06bb6a24dcb4effc5896615265cfff689ad0556
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-02-26 13:27:59 +0100

    created markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    (
+      item
+    ) => `<div class="gallery__item"><a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a></div>`
+  )
+  .join("");
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = markup;
