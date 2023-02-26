5aac8fdee3f2287655a1ef7b569fbf4dc7977f63
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-02-26 17:45:03 +0100

    simple done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    (item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  )
+  .join("");
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = markup;
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 250,
+  fadeSpeed: 250,
+  scrollZoom: false,
+  showCounter: false,
+});
