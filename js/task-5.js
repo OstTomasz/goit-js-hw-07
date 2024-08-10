1c2635357b97054a75971ab61b3e57c3eeeb108f
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-08-10 14:56:34 +0200

    task 5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const output = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  output.textContent = color;
+});
